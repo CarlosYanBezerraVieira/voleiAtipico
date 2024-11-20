4250751a21edaa9cc6e9a14d378f7303b5fc0e02
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-11-20 18:11:01 -0300

    feat: iniciando projeto

---
+++ android/app/src/main/kotlin/br/com/voleiAtipico/voleiatipico/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.voleiAtipico.voleiatipico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:voleiatipico/app/app_widget.dart';
+
+import 'app/core/get_registers.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupRegisters();
+  runApp(const AppWidget());
+}
