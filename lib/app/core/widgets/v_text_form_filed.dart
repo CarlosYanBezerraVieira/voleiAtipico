27aba498409f51ccaf4babe4923c5ea348d5d02f
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-11-21 14:58:36 -0300

    feat: create new widget vTextformFiled

---
+++ lib/app/core/widgets/v_text_form_filed.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:voleiatipico/app/core/theme/app_colors.dart';
+
+class VTextFormfiled extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String hintText;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const VTextFormfiled(
+      {super.key,
+      required this.controller,
+      required this.labelText,
+      required this.hintText,
+      this.onChanged,
+      this.validator});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(
+        fontSize: 16,
+        color: AppColors.black,
+      ),
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: AppColors.black,
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          fontSize: 14,
+          color: AppColors.yellowPrimary,
+        ),
+        filled: true,
+        fillColor: AppColors.yellowLite,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0), // Borda arredondada
+          borderSide: BorderSide.none, // Remove o contorno padrão
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(
+            color: AppColors.yellowPrimary, // Cor da borda ao focar
+            width: 2.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(
+            color: AppColors.yellowPrimary, // Cor da borda habilitada
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(
+            color: Colors.red, // Cor da borda ao dar erro
+            width: 2.0,
+          ),
+        ),
+      ),
+      controller: controller,
+      onChanged: onChanged,
+    );
+  }
+}
